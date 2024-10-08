a015a44566c44e2a6dab07bacb1f2659eaffced5
김형준 <gudwns051613@gmail.com>
2024-10-08 21:46:11 +0900

    HttpServlet 데이터 조회 방법

---
+++ servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,25 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/*
+    파라미터 전송 기능
+ */
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+        System.out.println("username = " + username);
+        System.out.println("age = " + age); 
+
+        response.getWriter().write("ok ");
+    }
+}
