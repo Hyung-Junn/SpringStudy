a3605360ab5e6f683a4ce4e7ae1c4278da8d4104
김형준 <gudwns051613@gmail.com>
2024-10-10 20:57:21 +0900

    servlet

---
+++ servlet/src/test/java/hello/servlet/basic/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,48 @@
+package hello.servlet.basic.domain.member;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        Member member = new Member("sex", 25);
+
+        //when
+        Member saveMember = memberRepository.save(member);
+
+        //then
+        Member findMember = memberRepository.findById(saveMember.getId());
+        assertThat(saveMember).isEqualTo(findMember);
+    }
+
+    @Test
+    void findAll() {
+        //given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 25);
+
+        //when
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        List<Member> members = memberRepository.findAll();
+        //then
+        assertThat(members.size()).isEqualTo(2);
+        assertThat(members).contains(member1, member2);
+    }
+}
