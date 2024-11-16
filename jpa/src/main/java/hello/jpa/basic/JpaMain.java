1db39c9d863eefff35c50e9e80cab6655a93e58a
김형준 <gudwns051613@gmail.com>
2024-11-16 17:45:03 +0900

    feat : 블럭 테스트 주석 추가

--- jpa/src/main/java/hello/jpa/basic/JpaMain.java
+++ jpa/src/main/java/hello/jpa/basic/JpaMain.java
@@ -5,5 +5,9 @@ import jakarta.persistence.Persistence;
 public class JpaMain {
     public static void main(String[] args) {
         //브랜치 테스트 주석 추가
+
+        /**
+         * 블럭 테스트 주석 추가
+         */
     }
 }
